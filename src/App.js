c5f17d0a8a397bd223661274db135b247483b131
rajat <rajatsrinet@gmail.com>
2024-12-06 18:44:31 +0530

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import Card from './card';
+import Card from './Card';
 
 function App() {
    
