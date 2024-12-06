3196415b8cef393f799f7a3676dc68463bdb4a5e
rajat <rajatsrinet@gmail.com>
2024-12-06 18:35:56 +0530

    first react-js projetc

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Card from './card';
+
+function App() {
+   
+  return (
+    <div className="App">
+      <Header/>
+      <Card/>
+      <Footer/>
+       
+    </div>
+  );
+}
+
+export default App;
