5185e6c24a97e151719252977cba7545d999ba04
rajat <rajatsrinet@gmail.com>
2025-01-27 10:59:32 +0530

    change in header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import Card from './Card';
+import { Container, Row, Col, Card,Button } from 'react-bootstrap';
 
 function App() {
    
   return (
     <div className="App">
-      <Header/>
-      <div className='container'>
-        <h1 className='text-danger'>Welcome to react js</h1>
-
-      </div>
-      <Card/>
+      <Header email = "test@gmail.com"/>
+       <Container fluid>
+        <Container>
+          <Row>
+            <Col className='col-12'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+       </Container> 
       <Footer/>
        
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
+    console.log(props)
   return (
     <div>Header</div>
   )
